9fa011291a731e86d09ed340a687b0ee028074d5
nefari0n <101109366+nefari0nzz@users.noreply.github.com>
2025-03-04 11:50:10 +0100

    Corrected order

--- Script_Files/Disable_Superfetch_Prefetch_MemoryCompression_PageCombining.bat
+++ Script_Files/Disable_Superfetch_Prefetch_MemoryCompression_PageCombining.bat
@@ -1,14 +1,14 @@
 @echo off
 echo Disabling SysMain (service), Memory Compression, Page Combining Prefetch...
 
-:: Stop and disable SysMain (service)
-sc stop SysMain
-sc config SysMain start=disabled
-
 :: Disable Memory Compression and Page Combining
 powershell -Command "Disable-MMAgent -mc"
 :: https://kaimi.io/en/2020/07/reading-another-process-memory-via-windows-10-page-combining-en/
 powershell -Command "Disable-MMAgent -PageCombining"
+
+:: Stop and disable SysMain (service)
+sc stop SysMain
+sc config SysMain start=disabled
 
 :: Disable Prefetch and Superfetch in Registry
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management\PrefetchParameters" /v EnablePrefetcher /t REG_DWORD /d 0 /f
